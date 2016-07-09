3372704809e1fc7453eec531944f0543dba37264
Radovan Zivkovic <pivonroll@gmail.com>
2016-07-09 13:13:42 +0200

    Replaced schema version functions with one function.
    Change-Id: I2ef62332170e43f158d34de0b59dc515ca723c86

--- src/plugins/vcprojectmanager/utils.cpp
+++ src/plugins/vcprojectmanager/utils.cpp
@@ -65,72 +65,17 @@ namespace Internal {
 namespace VisualStudioUtils {
 
 /*!
- * \return \b true if Visual Studio project at \a filePath is a Visual Studio 2003 project.
+ * \return \b true if Visual Studio project at \a filePath matches schema.
  * Otherwise it returns \b false.
  */
-bool checkIfVersion2003(const QString &filePath)
+
+bool checkIfProjectVersionMathcesSchema(Constants::SchemaVersion schemaVersion, const QString &filePath)
 {
-    QString vc2003Schema = VcSchemaManager::instance()->documentSchema(Constants::SV_2003);
+    QString vcSchema = VcSchemaManager::instance()->documentSchema(schemaVersion);
 
-    QTC_ASSERT(!vc2003Schema.isEmpty(), return false);
+    QTC_ASSERT(!vcSchema.isEmpty(), return false);
 
-    QFile schemaFile(vc2003Schema);
-    schemaFile.open(QIODevice::ReadOnly);
-
-    QXmlSchema schema;
-    schema.load(&schemaFile, QUrl::fromLocalFile(schemaFile.fileName()));
-
-    if (schema.isValid()) {
-        QFile file(filePath);
-        file.open(QIODevice::ReadOnly);
-
-        QXmlSchemaValidator validator(schema);
-        if (validator.validate(&file, QUrl::fromLocalFile(file.fileName())))
-            return true;
-    }
-
-    return false;
-}
-
-/*!
- * \return \b true if Visual Studio project at \a filePath is a Visual Studio 2005 project.
- * Otherwise it returns \b false.
- */
-bool checkIfVersion2005(const QString &filePath)
-{
-    QString vc2005Schema = VcSchemaManager::instance()->documentSchema(Constants::SV_2005);
-
-    QTC_ASSERT(!vc2005Schema.isEmpty(), return false);
-
-    QFile schemaFile(vc2005Schema);
-    schemaFile.open(QIODevice::ReadOnly);
-
-    QXmlSchema schema;
-    schema.load(&schemaFile, QUrl::fromLocalFile(schemaFile.fileName()));
-
-    if (schema.isValid()) {
-        QFile file(filePath);
-        file.open(QIODevice::ReadOnly);
-
-        QXmlSchemaValidator validator(schema);
-        if (validator.validate(&file, QUrl::fromLocalFile(file.fileName())))
-            return true;
-    }
-
-    return false;
-}
-
-/*!
- * \return \b true if Visual Studio project at \a filePath is a Visual Studio 2008 project.
- * Otherwise it returns \b false.
- */
-bool checkIfVersion2008(const QString &filePath)
-{
-    QString vc2008Schema = VcSchemaManager::instance()->documentSchema(Constants::SV_2008);
-
-    QTC_ASSERT(!vc2008Schema.isEmpty(), return false);
-
-    QFile schemaFile(vc2008Schema);
+    QFile schemaFile(vcSchema);
     schemaFile.open(QIODevice::ReadOnly);
 
     QXmlSchema schema;
@@ -153,11 +98,11 @@ bool checkIfVersion2008(const QString &filePath)
  */
 DocumentVersion getProjectVersion(const QString &projectPath)
 {
-    if (checkIfVersion2003(projectPath))
+    if (checkIfProjectVersionMathcesSchema(Constants::SV_2003, projectPath))
         return DV_MSVC_2003;
-    else if (checkIfVersion2005(projectPath))
+    else if (checkIfProjectVersionMathcesSchema(Constants::SV_2005, projectPath))
         return DV_MSVC_2005;
-    else if (checkIfVersion2008(projectPath))
+    else if (checkIfProjectVersionMathcesSchema(Constants::SV_2008, projectPath))
         return DV_MSVC_2008;
 
     return DV_UNRECOGNIZED;
